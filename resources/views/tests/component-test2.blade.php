40f90fdb83860b56f7c4c91022e7c4636335ed7b
soma_asahi <95704277+somaasahi@users.noreply.github.com>
2022-03-09 17:06:36 +0900

    sec02_componentClassBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,2 +1,7 @@
-test2
+<x-tests.app>
 <x-tests.card title="タイトル2" content="本文2" />
+
+componentteset2
+<x-test-class-base classBaseMessage="メッセージ"/>
+<x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更"/>
+</x-tests.app>
